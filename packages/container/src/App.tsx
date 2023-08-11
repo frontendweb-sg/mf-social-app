606c49cb6d4f088cce3d5e45672c93422f227a90
frontendweb-sg <frontendweb.sg@gmail.com>
2023-08-11 16:30:20 +0530

    update laoder

--- packages/container/src/App.tsx
+++ packages/container/src/App.tsx
@@ -2,12 +2,13 @@ import AppRoutes from "./router";
 import AppThemeProvider from "./providers/theme";
 import { BrowserRouter } from "react-router-dom";
 import { Suspense } from "react";
+import LinearProgress from "@mui/material/LinearProgress";
 
 const App = () => {
   return (
     <AppThemeProvider>
       <BrowserRouter>
-        <Suspense fallback={<h1>Loading....</h1>}>
+        <Suspense fallback={<LinearProgress />}>
           <AppRoutes />
         </Suspense>
       </BrowserRouter>
